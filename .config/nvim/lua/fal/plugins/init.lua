bff2037f17d57f279efad0c208ec9151f37c98bd
fal <naufalsuryalaga24@gmail.com>
2024-03-23 13:29:55 +0700

    add: dadbod

--- .config/nvim/lua/fal/plugins/init.lua
+++ .config/nvim/lua/fal/plugins/init.lua
@@ -4,6 +4,7 @@ return {
   "wuelnerdotexe/vim-astro",
   "gleam-lang/gleam.vim",
   "nvim-tree/nvim-web-devicons",
+  "tpope/vim-dadbod",
   {
     "bluz71/vim-moonfly-colors",
     name = "moonfly",
